b6ca351e877730f1b0e9e37f00f3b14d69360a60
Courtney Beckham <cjb374@cornell.edu>
2022-12-08 15:45:09 -0500

    fix dataset

---
+++ analyze_data.py
@@ -0,0 +1,8 @@
+# Goals:
+
+# 1) Implement the HK model on the data.
+# 2) Find summary statistics on data.
+# 3) Evaluate HIIT centrality, betweenness, eigenvector centrality, visualize those
+# 4) Graph neural network implementation.
+# 5) Run DeGroot model on the data and compare to HK.
+# 6) Stretch goal: bag of words on tweets.
