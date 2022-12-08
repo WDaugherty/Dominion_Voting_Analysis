a0634b6a4deb9cd6ae454a525ee1bca673ce6b51
Courtney Beckham <cjb374@cornell.edu>
2022-12-08 12:55:38 -0500

    add data preprocessing

--- pull_author_data.py
+++ pull_author_data.py
@@ -8,14 +8,10 @@ import time
 bearer_token = os.environ.get("BEARER_TOKEN")
 
 
-def create_url(next_token: str, iteration: int):
-    tweet_fields = "tweet.fields=author_id,context_annotations,conversation_id,created_at,entities,geo,id,in_reply_to_user_id,lang,public_metrics,referenced_tweets,reply_settings,source,text&user.fields=created_at,description,entities,id,location,name,public_metrics,username"
-    start_time = 'start_time=2020-11-04T00:00:00.000Z'
-    end_time = 'end_time=2020-11-14T00:00:00.000Z'
-    if iteration == 0:
-        url = "https://api.twitter.com/2/tweets/search/all?query=dominion%20voting%20maricopa&{}&{}&{}&max_results=100".format(start_time, end_time, tweet_fields)
-    else:
-        url = "https://api.twitter.com/2/tweets/search/all?query=dominion%20voting%20maricopa&{}&{}&{}&max_results=100&next_token={}".format(start_time, end_time, tweet_fields, next_token)
+def create_url(id: any):
+    user_fields = "user.fields=created_at,description,entities,id,location,name,public_metrics,username"
+    url = "https://api.twitter.com/2/users?ids={}&{}".format(id, user_fields)
+    print('create_url', url)
     return url
 
 
@@ -25,13 +21,12 @@ def bearer_oauth(r):
     """
 
     r.headers["Authorization"] = f"Bearer {bearer_token}"
-    r.headers["User-Agent"] = "v2TweetLookupPython"
+    r.headers["User-Agent"] = "v2UserLookupPython"
     return r
 
 
 def connect_to_endpoint(url):
     response = requests.request("GET", url, auth=bearer_oauth)
-    print(response.status_code)
     if response.status_code != 200:
         raise Exception(
             "Request returned an error: {} {}".format(
@@ -44,22 +39,26 @@ def connect_to_endpoint(url):
 def main():
     authors = open("authors.txt", "r")
     data = authors.read()
-    data_into_list = data.replace('\n', ' ').split(".")
-    print(data_into_list)
+    data_into_list = data.split('\n')
+
+    for i, user_id in enumerate(data_into_list):
+        if i > 2010:
+            print(i)
+            print('userid', user_id)
+            url = create_url(user_id)
+            print('url', url)
+
+            json_response = connect_to_endpoint(url)
+            json_object = json.dumps(json_response, indent=4, sort_keys=True)
+
+            filename = f"response_user{i}.json"
+            with open(filename, "w") as outfile:
+                outfile.write(json_object)
+
+            print(f'{i}/{len(data_into_list)}')
+            time.sleep(3.1)
+
     authors.close()
-
-    # count = 0
-    # for i in range(1, 3500, 10):
-    #     ids = ids[i:i+10]
-    #     ids = str(ids).replace(" ", "")[1:-1]
-    #     url = create_url(None, count, ids)
-    #     print(url)
-    #     json_response = connect_to_endpoint(url)
-    #     json_object = js.dumps(json_response, indent=4, sort_keys=True)
-
-    #     filename = f"response{count}_user.json"
-    #     with open(filename, "w") as outfile:
-    #         outfile.write(json_object)
 
 
 if __name__ == "__main__":
