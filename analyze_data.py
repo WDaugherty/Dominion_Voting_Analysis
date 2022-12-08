80663d8387e19b6c7485c30e34f5230315a115c4
Courtney Beckham <cjb374@cornell.edu>
2022-12-08 17:05:04 -0500

    add sentiment dataframe

--- analyze_data.py
+++ analyze_data.py
@@ -1,8 +1,10 @@
 # Goals:
 
-# 1) Implement the HK model on the data.
-# 2) Find summary statistics on data.
-# 3) Evaluate HIIT centrality, betweenness, eigenvector centrality, visualize those
-# 4) Graph neural network implementation.
-# 5) Run DeGroot model on the data and compare to HK.
-# 6) Stretch goal: bag of words on tweets.
+# 1) *90%* Implement the HK model on the data.
+# 2) (Courtney) Find summary statistics on data.
+# 3) *Done* Evaluate HIIT centrality, betweenness, eigenvector centrality, visualize those
+# 4) (Will) Graph neural network implementation.
+# 5) (Will) Run DeGroot model on the data and compare to HK.
+# 6) *Done* Stretch goal: bag of words on tweets.
+# 7) *Done* Run PageRank on the graphs
+# 8) (Will) Check on if random gossipping is possible.
